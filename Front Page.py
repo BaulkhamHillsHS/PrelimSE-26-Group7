7e54b40d7d6cf7fe0da90b8994c31f622c7629f1
114514 <97tycoonga5@gmail.com>
2026-06-09 11:10:54 +0800

    Added working top layer for watchlist and search (unfinished), edited TV show file names

--- Front Page.py
+++ Front Page.py
@@ -1,81 +1,110 @@
 import os
-import tkinter
+import tkinter as tk
 import customtkinter as ctk
 from PIL import Image
+
+class Watchlist(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Watchlist")
+        self.geometry("1280x900")
+        self.label = ctk.CTkLabel(self, text="Watchlist", fg_color="#6D268B", width=150, height=25)
+        self.label.grid(column=1, row=1, padx=10, pady=10)
+
+class Search(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Search")
+        self.geometry("1280x900")
+        self.label = ctk.CTkLabel(self, text="Search", fg_color="#6D268B", width=300, height=50, font=("Comic Sans MS", 24))
+        self.label.grid(column=1, row=1, padx=10, pady=10)
 
 class App(ctk.CTk):
 
     global tv
 
-    def __init__(root, **kwargs):
-        super().__init__(**kwargs)
-        root.title("Yuri Watching")
-        root.geometry("2560x1800")
-        root._build_ui()
+    def __init__(self):
+        super().__init__()
+        self.title("Yuri Watching")
+        self.geometry("2560x1800")
+        self.toplevel_window = None
+        self.toplevel_window2 = None
+        self._build_ui()
 
-
-
-
-    def _build_ui(root):
+    def _build_ui(self):
         
         x = 0
-        root._openwatchlist = None
         tvs = []
 
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/stfu.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/kessokulastwinter.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigakfc.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/shikanokonokonokokoshitantan.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taiga.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taiga.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
-        tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
+        tvs.append(ctk.CTkImage(Image.open('luckystar.jpg'), size=(200,300)))
+        tvs.append(ctk.CTkImage(Image.open('btr.jpg'), size=(200,300)))
+        tvs.append(ctk.CTkImage(Image.open('ddlc.jpg'), size=(200,300)))
+        tvs.append(ctk.CTkImage(Image.open('toradora.jpg'), size=(200,300)))
+        tvs.append(ctk.CTkImage(Image.open('shikanokonokonokokoshitantan.jpg'), size=(200,300)))
+        #tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taiga.jpg'), size=(200,300)))
+        #tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taiga.jpg'), size=(200,300)))
+        #tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
+        #tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
+        #tvs.append(ctk.CTkImage(Image.open('/Users/ryanlam/Desktop/i need this/taigashutup.jpg'), size=(200,300)))
 
         tvname = []
 
         tvname.append("Lucky Star")
-        tvname.append("All of the Above")
-        tvname.append("Toradora")
+        tvname.append("Bocchi The Rock!")
+        tvname.append("DDLC")
         tvname.append("Toradora")
         tvname.append("Shikanoko Nokonoko Koshitantan")
-        tvname.append("Toradora")
-        tvname.append("Toradora")
-        tvname.append("Toradora")
-        tvname.append("Toradora")
-        tvname.append("Toradora")
+        #tvname.append("Toradora")
+        #tvname.append("Toradora")
+        #tvname.append("Toradora")
+        #tvname.append("Toradora")
+        #tvname.append("Toradora")
 
-        root.label = ctk.CTkLabel(root, text="You're Watching", fg_color="#325EE2", text_color="white", width=200, height=50, font=("Arial", 24))
-        root.label.grid(padx=30, pady=20, column=0, row=0, columnspan=2)
+        self.label = ctk.CTkLabel(self, text="You're Watching", fg_color="#325EE2", text_color="white", width=200, height=50, font=("Comic Sans MS", 24))
+        self.label.grid(padx=0, pady=0, column=0, row=0, columnspan=2)
 
-        root.label = ctk.CTkLabel(root, text="Front Page TV Shows", fg_color="#718CDD", width=150, height=25)
-        root.label.grid(padx=30, pady=0, column=0, row=1, columnspan=2)
+        self.label = ctk.CTkLabel(self, text="Front Page TV Shows", fg_color="#718CDD", width=150, height=25, font=("Comic Sans MS", 12))
+        self.label.grid(padx=0, pady=0, column=0, row=1, columnspan=2)
 
-        root.btn = ctk.CTkButton(root, text="Watchlist", command=root._openwatchlist)
-        root.btn.grid(column=6,row=1)
+        self.btn = ctk.CTkButton(self, text="Watchlist", command=self._openwatchlist, font=("Comic Sans MS", 14))
+        self.btn.grid(column=6,row=1)
 
+        self.btn = ctk.CTkButton(self, text="Search", command=self._opensearch)
+        self.btn.grid(column=3, row=1, columnspan=2)
 
-        for i in range(x, 5+x):
-            root.label = ctk.CTkLabel(root, text="", image=tvs[i])
-            root.label.grid(padx=10, pady=20, column=i+1, row=2)
-            root.btn = ctk.CTkButton(root, text=tvname[i], command="")
-            root.btn.grid(padx=10, pady=0, column=i+1, row=3)
+        thingyb = []
+
+        #for i in range(x, 5+x):
+        #    self.label = ctk.CTkLabel(self, text="", image=tvs[i])
+        #    self.label.grid(padx=10, pady=20, column=i+1, row=2)
+        #    self.btn = ctk.CTkButton(self, text=tvname[i])
+        #    self.btn.grid(padx=10, pady=0, column=i+1, row=3)
+        #    i+=1
+
+        for i in range(x, 6+x):
+            self.label=ctk.CTkLabel(self, text="", image=tvs[i])
+            self.label.grid(padx=10, pady=20, column=i+1, row=2)
+            thingyb.append(ctk.CTkButton(self, text=tvname[i]))
+            self.btn.grid(padx=10, pady=0, column=i+1, row=3)
             i+=1
 
-    def _openwatchlist(root):
-        if root._openwatchlist is None or not root._openwatchlist.winfo_exists():
-            root._openwatchlist = Watchlist(root)
+
+    def _openwatchlist(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = Watchlist(self)
         else:
-            root._openwatchlist.focus()
+            self.toplevel_window.focus()
 
-class Watchlist(ctk.CTkToplevel):
-    def __init__(root, **kwargs):
-        super().__init__(**kwargs)
-        root.title("Watchlist")
-        root.geometry("2560,1800")
-        root.label = ctk.CTkLabel(root, "Watchlist")
+    def _opensearch(self):
+        if self.toplevel_window2 is None or not self.toplevel_window2.winfo_exists():
+            self.toplevel_window2 = Search(self)
+        else:
+            self.toplevel_window2.focus()
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+
+
+
+
+# if __name__ == "__main__":
+app = App()
+app.mainloop()
